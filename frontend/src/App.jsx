70601a5470e810b10a4c22eb5e1b7a70f7eb4228
kennedy muthuri <muthurikennedy082@gmail.com>
2022-10-12 17:00:36 +0300

    created database schemas

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,4 +1,6 @@
 import{BrowserRouter as Switch, Routes, Route} from 'react-router-dom';
+import Admin from './views/admin';
+import Stock from './views/admin.panel';
 import Home from './views/home';
 
 function App() {
@@ -7,6 +9,8 @@ function App() {
       <Switch>
         <Routes>
           <Route exact path='/' element={<Home/>}/>
+          <Route exact path='/admin' element={<Admin/>}/>
+          <Route exact path ='/stock' element={<Stock/>}/>
         </Routes>
       </Switch>
     </div>
--- frontend/src/components/navbar.jsx
+++ frontend/src/components/navbar.jsx
@@ -1,4 +1,4 @@
-import {Drawer, Grid, IconButton, List, ListItem}from '@mui/material';
+import {AppBar, Button, Drawer, Grid, IconButton, List, Tab, Tabs, Toolbar,Box, ListItemButton, ListItemIcon, ListItemText, Dialog, DialogTitle, Typography, DialogContent}from '@mui/material';
 import DehazeOutlinedIcon from '@mui/icons-material/DehazeOutlined';
 import { useState } from 'react';
 import AddShoppingCartOutlinedIcon from '@mui/icons-material/AddShoppingCartOutlined';
@@ -10,29 +10,100 @@ import LocalShippingOutlinedIcon from '@mui/icons-material/LocalShippingOutlined
 import LocationOnOutlinedIcon from '@mui/icons-material/LocationOnOutlined';
 import ChairOutlinedIcon from '@mui/icons-material/ChairOutlined';
 import WomanOutlinedIcon from '@mui/icons-material/WomanOutlined';
+import { useNavigate } from 'react-router-dom';
 
 const Navbar = () => {
+    const navigate= useNavigate();
     const[drawer, setDrawer]= useState(false);
+    const[value, setValue]= useState();
+    const [open, setOpen]= useState(false);
     const handleDrawer= function(){
         setDrawer(true);
     }
+
+    const handleModal= function(){
+        setOpen(true);
+    }
+    const handleAdmin = function(){
+        navigate('/admin');
+    }
     return ( 
         <div>
-            <Grid container>
-                <Grid item>
-                    <IconButton onClick={handleDrawer}>
-                        <DehazeOutlinedIcon/>
-                    </IconButton>
-                </Grid>
-            </Grid>
+            <AppBar sx={{backgroundColor:'#0096FF'}}>
+                <Toolbar>
+                    <Grid container spacing={2} SX={{placeItems:'center'}}>
+                        <Grid item xs={1}>
+                           <IconButton onClick={handleDrawer} color='inherit'>
+                               <DehazeOutlinedIcon/>
+                           </IconButton>
+                        </Grid>
+                        <Grid item xs={1}></Grid>
+                        <Grid item xs={6}>
+                            <Tabs value={value}textColor='inherit' indicatorColor='secondary'onChange={(e,val)=>setValue(val)}>
+                                <Tab label='HOME'/>
+                                <Tab label='CONTACTS'/>
+                                <Tab label='ABOUT US'/>
+                            </Tabs>
+                        </Grid>
+                        <Grid item xs={3}>
+                            <Box display='flex'>
+                                <IconButton sx={{marginLeft:'auto', color:'white'}}>
+                                    <ShoppingCartOutlinedIcon/>
+                                </IconButton>
+                            <Button variant='contained' color='success' sx={{display:'flex',marginLeft:3}}
+                            onClick={handleModal}
+                            >LOGIN</Button>
+                            </Box>
+                        </Grid>
+                    </Grid>
+                </Toolbar>
+            </AppBar>
             <Drawer
             anchor='left'
             open={drawer}
+            sx={{width:window.innerWidth*0.75}}
             >
                 <List>
-                    <ListItem button>Learning drawer</ListItem>
+                   <ListItemButton  divider onClick={()=>setDrawer(false)}>
+                        <ListItemText>CLOSE  X</ListItemText>
+                   </ListItemButton >
+                    <ListItemButton divider>
+                        <ListItemIcon>
+                            <ComputerOutlinedIcon/>
+                        </ListItemIcon>
+                        <ListItemText>Computers</ListItemText>
+                    </ListItemButton>
+                    <ListItemButton divider>
+                        <ListItemIcon>
+                            <WomanOutlinedIcon/>
+                        </ListItemIcon>
+                        <ListItemText>Fashion</ListItemText>
+                    </ListItemButton>
+                    <ListItemButton divider>
+                        <ListItemIcon>
+                            <ChairOutlinedIcon/>
+                        </ListItemIcon>
+                        <ListItemText>Furniture</ListItemText>
+                    </ListItemButton>
+                    <ListItemButton divider>
+                        <ListItemIcon>
+                            <ComputerOutlinedIcon/>
+                        </ListItemIcon>
+                        <ListItemText>Electronics</ListItemText>
+                    </ListItemButton>
+                    <Button variant='contained'color='primary'
+                    sx={{margin:6}} onClick={handleAdmin}
+                    >Admin Login</Button>
                 </List>
             </Drawer>
+            <Dialog open={open} sx={{ alignItems:'center'}}>
+                <DialogTitle sx={{fontFamily:'monospace',display:'flex', justifyContent:'center'}}>
+                    <Typography>LOGIN</Typography>
+                </DialogTitle>
+                <DialogContent>
+                    <Button variant='contained'>LOGIN USING GOOGLE</Button>
+                </DialogContent>
+            </Dialog>
         </div>
      );
 }
